eac637d707a96d87a88ea4656d8e4baabca96f1b
Shreyas Balachandran <shreyas.bba@gmail.com>
2021-03-10 02:54:02 +0530

    Documentation update

--- src/controller/imageAPI.ts
+++ src/controller/imageAPI.ts
@@ -1,5 +1,6 @@
 import { imageAPIData } from "../interfaces/imageAPI"
 
+// API Function to fetch photos from Picsum and return them to be used.
 export const imageAssets = (url = 'https://picsum.photos/v2/list?limit=10') : Promise<imageAPIData[]> => {
     return new Promise((res, rej) => {
         const images = fetch(url).then(response => response.json())
